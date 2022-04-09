68f46e8125d527416253c6a7fce7419b926db23c
emilyreddy <emreddy@icloud.com>
2022-04-08 20:18:48 -0400

    functioning html

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,13 @@ fetch(requestUrl)
 .then(function(data){
     console.log(data);
 })
+
+var requestUrl = "https://api.edamam.com/api/nutrition-data?app_id=cd587999&app_key=d7e8f51d5b855c15479cee34dfba35a4&nutrition-type=cooking&ingr=1%20apple";
+fetch(requestUrl)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+  console.log(data2);  
+})
+
